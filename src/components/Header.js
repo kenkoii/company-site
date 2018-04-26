89966a8dc9792f97f3f97aefd06e088b44e0c796
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-04-26 17:23:29 +0800

    Refactored SVG elems to own component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,51 +2,71 @@ import React from 'react'
 import Link from 'gatsby-link'
 import LanguageSelector from './SelectLanguage';
 
-const Header = (props) => {
-  const selectedLang = props.langs.filter(lang => lang.selected == true)[0];
-  return (
-    <nav className="nav nav--mobile">
-      <div className="container">
-        <div className="nav__group nav--left">
-          <div className="nav__brand">
-            <Link to={`/${selectedLang.langKey}`}>F R E C R E</Link>
+export class Header extends React.Component {
+  constructor(props){
+    super(props);
+    console.log(props);
+    const selectedLang = props.langs.filter(lang => lang.selected == true)[0];
+    this.state = {
+      selectedLang : selectedLang,
+      sideBarActive: false
+    }
+  }  
+
+  onToggle = () => {
+    this.setState({sideBarActive: !this.state.sideBarActive});
+  }  
+
+  render() {
+    return (
+      <nav className="nav nav--mobile">
+        <div className="container">
+          <div className="nav__group nav--left">
+            <div className="nav__brand">
+              <Link to={`/${this.state.selectedLang.langKey}`}>F R E C R E</Link>
+            </div>
           </div>
+          <div className={`nav__group nav--right nav--w70p nav--sidebar ${this.state.sideBarActive?'active':''}`}>
+            <ul className="nav__group_link">
+              <li className="nav__link nav--dropdown game">
+                <a>GAMES</a>
+                <ul className="nav__dropdown">
+                  <li>
+                    <Link to={`/${this.state.selectedLang.langKey}/wordcosmos/`} onClick={this.onToggle}>Eigomonogatari</Link>
+                  </li>
+                  <li>
+                    <Link to={`/${this.state.selectedLang.langKey}/wordcosmos/`} onClick={this.onToggle}>Word Cosmos</Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav__link">
+                <Link to="">CAREERS</Link>
+              </li>
+              <li className="nav__link">
+                <Link to="">BLOG</Link>
+              </li>
+              <li className="nav__link">
+                <Link to="">CONTACT</Link>
+              </li>
+            </ul>
+            <LanguageSelector langs={this.props.langs}/>
+          </div>
+          <div 
+            className={`nav__sidebar--overlay ${this.state.sideBarActive?'active':''}`}
+            onClick={this.onToggle}
+          ></div>
         </div>
-        <div className="nav__group nav--right nav--w70p nav--sidebar">
-          <ul className="nav__group_link">
-            <li className="nav__link nav--dropdown game">
-              <Link to="#">GAMES</Link>
-              <ul className="nav__dropdown">
-                <li>
-                  <Link to={`/${selectedLang.langKey}/wordcosmos/`}>Eigomonogatari</Link>
-                </li>
-                <li>
-                  <Link to={`/${selectedLang.langKey}/wordcosmos/`}>Word Cosmos</Link>
-                </li>
-              </ul>
-            </li>
-            <li className="nav__link">
-              <Link to="#">CAREERS</Link>
-            </li>
-            <li className="nav__link">
-              <Link to="#">BLOG</Link>
-            </li>
-            <li className="nav__link">
-              <Link to="#">CONTACT</Link>
-            </li>
-          </ul>
-          <LanguageSelector langs={props.langs}/>
-        </div>
-      </div>
-      <button
-        className="nav-burger nav--sidebutton nav--right"
-        type="button"
-        name="button">
-        <span className="nav-burger__top"></span>
-        <span className="nav-burder__patty"></span>
-        <span className="nav-burger__bottom"></span>
-      </button>
-    </nav>
-  )
+        <button
+          className="nav-burger nav--sidebutton nav--right"
+          type="button"
+          onClick={this.onToggle}
+          name="button">
+          <span className="nav-burger__top"></span>
+          <span className="nav-burder__patty"></span>
+          <span className="nav-burger__bottom"></span>
+        </button>
+      </nav>
+    )
+  }
 }
 export default Header;
---
+++ src/components/frecre/WhatWeDoSvg.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {FormattedMessage} from 'react-intl';
+
+const WhatWeDoSvg = (props) => {
+    return(
+        <div className="section-info">
+              <div className={`section-info__image section--svg ${props.isVisible?'active animate':''}`}>
+                <svg id='Layer_1' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'>
+                <style type="text/css">
+                    {`.st0-wwd{fill:#B2B2B2;}
+                    .st1-wwd{fill:none;stroke:#F45B3D;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}
+                    .st2-wwd{fill:none;stroke:#E5E5E5;stroke-width:10;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}`}
+                </style>
+                  <g id='XMLID_84_'>
+                      <circle id='XMLID_26_' className='st0-wwd' cx='245.7' cy='288.8' r='10.4'
+                      />
+                      <circle id='XMLID_27_' className='st0-wwd' cx='264.5' cy='306' r='6.8' />
+                      <circle id='XMLID_28_' className='st0-wwd' cx='250.9' cy='317.9' r='5.2' />
+                      <circle id='XMLID_29_' className='st0-wwd' cx='260.5' cy='329.7' r='2.8' />
+                      <g id='XMLID_2_'>
+                          <path id='XMLID_20_' className='st1-wwd' d='M254.9,182c0,0-27.2-0.6-27.2,87.4c0,0,7,0,56.7,0c0-88-27.2-87.4-27.2-87.4'
+                          />
+                          <path id='XMLID_25_' className='st1-wwd' d='M283.2,239.1c0,0,24.2-3.1,24.2,30.3h-22.9'
+                          />
+                          <path id='XMLID_24_' className='st1-wwd' d='M227.8,239.1c0,0-24.2-3.1-24.2,30.3h22.9'
+                          />
+                          <circle id='XMLID_30_' className='st1-wwd' cx='256.1' cy='218.4' r='7.3' />
+                      </g>
+                      <g id='XMLID_1_'>
+                          <path id='XMLID_9_' className='st1-wwd' d='M312.7,404.1H198.2c-16.5,0-30-13.5-30-30V137.9c0-16.5,13.5-30,30-30h114.5 c16.5,0,30,13.5,30,30v236.2C342.7,390.6,329.2,404.1,312.7,404.1z'
+                          />
+                          <line id='XMLID_18_' className='st1-wwd' x1='168.2' y1='146.5' x2='342.7'
+                          y2='146.5' />
+                          <line id='XMLID_17_' className='st1-wwd' x1='168.2' y1='358.5' x2='342.7'
+                          y2='358.5' />
+                          <line id='XMLID_32_' className='st1-wwd' x1='227.1' y1='377.8' x2='283.8'
+                          y2='377.8' />
+                          <line id='XMLID_33_' className='st1-wwd' x1='242.2' y1='126.2' x2='268.7'
+                          y2='126.2' />
+                      </g>
+                      <g id='XMLID_58_'>
+                          <circle id='XMLID_57_' className='st2-wwd' cx='255.5' cy='256' r='227.7' />
+                          <path id='XMLID_56_' className='st2-wwd' d='M354.5,32c26.4,11.7,50.4,27.9,70.9,47.7'
+                          />
+                          <path id='XMLID_55_' className='st2-wwd' d='M27.8,346.1c-11.1-27.9-17.1-58.3-17.1-90.1c0-135.2,109.6-244.8,244.8-244.8 c19.4,0,38.4,2.3,56.5,6.6'
+                          />
+                          <path id='XMLID_54_' className='st2-wwd' d='M469,136.2c19.9,35.4,31.3,76.3,31.3,119.8c0,135.2-109.6,244.8-244.8,244.8 c-55.3,0-106.2-18.3-147.2-49.2'
+                          />
+                      </g>
+                      <g id='XMLID_59_'>
+                          <circle id='XMLID_64_' className='st0-wwd' cx='123.3' cy='361.3' r='19.8'
+                          />
+                          <circle id='XMLID_63_' className='st0-wwd' cx='92.2' cy='325.7' r='12' />
+                          <circle id='XMLID_62_' className='st0-wwd' cx='113.8' cy='291.5' r='12' />
+                          <circle id='XMLID_61_' className='st0-wwd' cx='85.2' cy='279.4' r='7.7' />
+                          <circle id='XMLID_60_' className='st0-wwd' cx='104.3' cy='259.9' r='5.1' />
+                      </g>
+                      <g id='XMLID_65_'>
+                          <circle id='XMLID_70_' className='st0-wwd' cx='380.9' cy='366.5' r='19.8'
+                          />
+                          <circle id='XMLID_69_' className='st0-wwd' cx='412' cy='330.9' r='12' />
+                          <circle id='XMLID_68_' className='st0-wwd' cx='390.4' cy='296.7' r='12' />
+                          <circle id='XMLID_67_' className='st0-wwd' cx='419' cy='284.6' r='7.7' />
+                          <circle id='XMLID_66_' className='st0-wwd' cx='399.9' cy='265.1' r='5.1' />
+                      </g>
+                      <circle id='XMLID_71_' className='st0-wwd' cx='180.2' cy='434.4' r='14.9'
+                      />
+                      <circle id='XMLID_72_' className='st0-wwd' cx='227' cy='430.9' r='11.3' />
+                      <circle id='XMLID_73_' className='st0-wwd' cx='275' cy='453.2' r='14.5' />
+                      <circle id='XMLID_74_' className='st0-wwd' cx='322.4' cy='436.2' r='18' />
+                      <circle id='XMLID_75_' className='st0-wwd' cx='267.2' cy='422.1' r='6.7' />
+                      <circle id='XMLID_76_' className='st0-wwd' cx='238.9' cy='456.8' r='3.6' />
+                      <circle id='XMLID_77_' className='st0-wwd' cx='361.1' cy='411.4' r='7.3' />
+                      <circle id='XMLID_78_' className='st0-wwd' cx='134.5' cy='407.5' r='8.6' />
+                  </g>
+              </svg>
+              </div>
+              <h1 className="section-info__text section--text-center"><FormattedMessage id="whatWeDo"/></h1>
+              <p className="section-info__text section--text-gray"><FormattedMessage id="whatWeDoDescription"/></p>
+            </div>
+    )
+}
+
+export default WhatWeDoSvg;
