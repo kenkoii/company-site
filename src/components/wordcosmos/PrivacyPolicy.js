24ce6fe83beafe51de423ba3dd260fe081da1e32
Jan Kenith Johannessen <jankenith.johannessen@gmail.com>
2018-08-30 11:34:40 +0800

    Added privacy policy and terms content

--- src/components/wordcosmos/PrivacyPolicy.js
+++ src/components/wordcosmos/PrivacyPolicy.js
@@ -10,7 +10,193 @@ const PrivacyPolicy = (props) => {
                         <div className="col-sm">
                             <div className="section-info">
                                 <h1 className="title__text-md">Privacy Policy</h1>
-                                <h3 className="section--text-white">this page is under construction</h3>
+                                <div className="section--text-white">
+                                    <p>
+                                        <strong>Effective Date: 1 August 2018</strong>
+                                    </p>
+                                    <p>Thank you for learning English through Word Cosmos! Our Privacy Policy
+                                        discusses the following:</p>
+                                    <ul>
+                                        <li>How we collect your personal data and why we do so</li>
+                                        <li>How we use your personal data, and</li>
+                                        <li>The choices you have about providing your personal data.</li>
+                                    </ul>
+                                    <p>This Privacy Policy applies to FreCre Inc.&rsquo;s games, websites and
+                                        related services, which we here collectively call the Service. When necessary,
+                                        we may update this Privacy Policy and let you know by posting an announcement on
+                                        the Service prior to the change becoming effective. Your continued use of the
+                                        Service after the effective date will then be subject to the new Privacy Policy.</p>
+                                    <p>
+                                        <strong>Data We Collect
+                                        </strong>
+                                    </p>
+                                    <p>
+                                        <strong>Data you provide us</strong>
+                                    </p>
+                                    <ul>
+                                        <li>Player username</li>
+                                        <li>Your messages to the Service, such as chat logs and player support tickets</li>
+                                        <li>Other data you choose to provide (such as data to identify a lost account)</li>
+                                    </ul>
+                                    <p>
+                                        <strong>Data we collect automatically</strong>
+                                    </p>
+                                    <ul>
+                                        <li>Data about your account and game progress</li>
+                                        <li>Your IP address and mobile device identifiers (such as your device ID, MAC
+                                            address, IMEI)</li>
+                                        <li>Data about your device, such as device model name, operating system, and
+                                            language</li>
+                                        <li>General location data</li>
+                                        <li>Data about your use of the Service, such as gameplay data and your
+                                            interactions with features and other players on the Service</li>
+                                    </ul>
+                                    <p>
+                                        <strong>Why We Collect Your Data</strong>
+                                    </p>
+                                    <p>
+                                        <strong>To make The Service work</strong>
+                                    </p>
+                                    <p>We process data necessary to</p>
+                                    <ul>
+                                        <li>Allow you to play our games and access The Service</li>
+                                        <li>Operate The Service</li>
+                                        <li>Verify and confirm payments</li>
+                                        <li>Provide and deliver products and services you request</li>
+                                        <li>Send you service-related notifications</li>
+                                    </ul>
+                                    <p>
+                                        <strong>To make The Service more engaging and suitable for our players</strong>
+                                    </p>
+                                    <ul>
+                                        <li>Update and develop player profiles</li>
+                                        <li>Develop and improve The Service and player experience</li>
+                                        <li>Customize your Service experience</li>
+                                        <li>Respond to your comments and questions and provide player support</li>
+                                        <li>Provide you offers in The Service as well as in other websites and services,
+                                            and by e-mail</li>
+                                        <li>Send you related information, such as updates, security alerts, and support
+                                            messages</li>
+                                    </ul>
+                                    <h4>
+                                        <strong>To keep the Service safe and fair</strong>
+                                    </h4>
+                                    <p>Ensuring fair play in The Service is important to us. For more information on
+                                        our acceptable use policy, see our Terms of Service.</p>
+                                    <p>In order to keep The Service safe and fair, to fight fraud and ensure
+                                        acceptable use otherwise, we have a legitimate interest to process necessary
+                                        data to</p>
+                                    <ul>
+                                        <li>Analyze and monitor use of The Service</li>
+                                        <li>Take action against fraudulent or misbehaving players</li>
+                                    </ul>
+                                    <h4>
+                                        <strong>To analyze, profile, and segment</strong>
+                                    </h4>
+                                    <p>In all of the above cases and purposes, we may analyze, profile and segment
+                                        all collected data.</p>
+                                    <h4>
+                                        <strong>With your consent</strong>
+                                    </h4>
+                                    <p>With your consent, we may process your data for additional purposes, such as
+                                        using your GPS location to show you local events.</p>
+                                    <h3>
+                                        <strong>Who Can See Your Data</strong>
+                                    </h3>
+                                    <p>Apart from FreCre, Inc., your data can be accessed by others in the following
+                                        situations:</p>
+                                    <h3>
+                                        <strong>FreCre, Inc. partners</strong>
+                                    </h3>
+                                    <p>FreCre, Inc. has partners to perform services for us. These partners process
+                                        your data only at and according to FreCre, Inc.&rsquo;s instructions to provide
+                                        Thee Service, such as hosting, player support, advertising, analytics and fraud
+                                        prevention.</p>
+                                    <h4>
+                                        <strong>Other companies and public authorities</strong>
+                                    </h4>
+                                    <p>In order to combat fraud and illegal activity, we may exchange data with
+                                        other companies and organizations and provide it to public authorities in
+                                        response to lawful requests.</p>
+                                    <p>We may also disclose your data based on your consent, to comply with the law
+                                        or to protect the rights, property or safety of us, our players or others.</p>
+                                    <h4>
+                                        <strong>Advertising and Social Media partners</strong>
+                                    </h4>
+                                    <p>The Service includes features from our partners, such as social media
+                                        interaction tools and in-game advertising. These partners may access your data
+                                        and operate under their own privacy policies.</p>
+                                    <h3>
+                                        <strong>International Data Transfers</strong>
+                                    </h3>
+                                    <p>Our Service is global by nature and your data can therefore be transferred to
+                                        anywhere in the world. Because different countries may have different data
+                                        protection laws than your own country, we take steps to ensure adequate
+                                        safeguards are in place to protect your data as explained in this Policy.
+                                        Adequate safeguards that our partners may use include standard contractual
+                                        clauses approved by EU Commission and the Privacy Shield certification in case
+                                        of transfers to the USA.</p>
+                                    <h3>
+                                        <strong>Your Rights and Options</strong>
+                                    </h3>
+                                    <h4>
+                                        <strong>Opt-out of marketing emails and other direct marketing</strong>
+                                    </h4>
+                                    <p>You may opt-out of receiving promotional communications, such as marketing
+                                        e-mails from us by following the instructions in such communications.</p>
+                                    <h4>
+                                        <strong>Access the personal data we hold about you</strong>
+                                    </h4>
+                                    <p>If you request, we will provide you a copy of your personal data in an
+                                        electronic format.</p>
+                                    <h4>
+                                        <strong>Your other rights</strong>
+                                    </h4>
+                                    <p>You also have the right to correct your data, have your data deleted, object
+                                        how we use or share your data, and restrict how we use or share your data. You
+                                        can always withdraw your consent, for example by turning off GPS location
+                                        sharing in your mobile device settings.</p>
+                                    <p>We will respond to all requests within a reasonable timeframe. You may also
+                                        contact your local data protection authority within the European Economic Area
+                                        for unresolved complaints.</p>
+                                    <h3>
+                                        <strong>Cookies and similar technologies
+                                        </strong>
+                                    </h3>
+                                    <p>Like most online services, we and our partners use cookies and similar
+                                        technologies to provide and personalize The Service, analyse use, target
+                                        advertisements and prevent fraud. You can disable cookies in your browser
+                                        settings, but some parts of The Service may then not function properly.</p>
+                                    <h3>
+                                        <strong>How we protect your data</strong>
+                                    </h3>
+                                    <h4>
+                                        <strong>Security Safeguards</strong>
+                                    </h4>
+                                    <p>In order to help ensure a secure and safe player experience, we are
+                                        continuously developing and implementing administrative, technical and physical
+                                        security measures to protect your data from unauthorized access or against loss,
+                                        misuse or alteration.</p>
+                                    <h4>
+                                        <strong>Data retention</strong>
+                                    </h4>
+                                    <p>We retain your data for as long as your account is active or as needed to
+                                        provide you The Service.</p>
+                                    <p>Note that if you ask us to remove your personal data, we will retain your
+                                        data as necessary for our legitimate business interests, such as to comply with
+                                        our legal obligations, resolve disputes, and enforce our agreements.</p>
+                                    <h3>
+                                        <strong>Age limits</strong>
+                                    </h3>
+                                    <p>We do not knowingly collect or solicit personal data about or direct or
+                                        target interest based advertising to anyone under the age of 13 or knowingly
+                                        allow such persons to use our Services. If you are under 13, please do not send
+                                        any data about yourself to us, including your name, address, telephone number,
+                                        or email address. No one under the age of 13 may provide any personal data. If
+                                        we learn that we have collected personal data about a child under age 13, we
+                                        will delete that data as quickly as possible. If you believe that we might have
+                                        any data from or about a child under the age of 13, please contact us.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -19,5 +205,5 @@ const PrivacyPolicy = (props) => {
         </div>
     )
 }
-                            
+
 export default PrivacyPolicy
